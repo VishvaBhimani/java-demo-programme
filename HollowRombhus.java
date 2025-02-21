039b20dd1297d516f6101a6b2be4298b07d1cf2d
VishvaBhimani <vishvabhimani123@gmail.com>
2025-02-21 22:11:12 +0530

    pattern

---
+++ HollowRombhus.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class HollowRombhus{
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter number of rows = ");
+      int n = sc.nextInt();
+      
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=(n-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j=1 ; j<=n ; j++){
+                if(i==1 || i==n){
+                    System.out.print("*");
+                }
+                else{
+                    if(j==1 || j==n){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+     
+
+
+      
+           
+}
+}
